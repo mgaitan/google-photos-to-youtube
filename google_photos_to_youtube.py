f809d2ea1cccec88159c15e84e91086b0cae19ec
Martín Gaitán <gaitan@gmail.com>
2023-01-15 11:59:32 -0300

    allow delete. DB with full mapping protocol

--- google_photos_to_youtube.py
+++ google_photos_to_youtube.py
@@ -1,4 +1,4 @@
-import functools
+import collections.abc
 import getpass
 import http.client as httplib
 import json
@@ -100,9 +100,10 @@ def create_db_image(session, album_id):
     return response.json()["newMediaItemResults"][0]["mediaItem"]
 
 
-class DB:
+class DB(collections.abc.MutableMapping):
     """
-    A Borg (shared state instance) dict-like to permanently store already migrated videos
+    A Borg (shared state) dict-like object to permanently store already migrated videos. 
+    It maps gphoto url to youtube urls. 
 
     It's stored as a json blob in the description of the first item of the
     album "migrated-to-youtube" that's created if needed.
@@ -123,12 +124,21 @@ class DB:
     def __getitem__(self, key):
         return self.data[key]
 
-    def __contains__(self, key):
-        return key in self.data
-
     def __setitem__(self, key, value):
         self.data[key] = value
+        self._commit()
 
+    def __delitem__(self, key):
+        del self.data[key]
+        self._commit()
+    
+    def __len__(self):
+        return len(self.data)
+
+    def __iter__(self):
+        yield from self.data
+
+    def _commit(self):
         self.session.patch(
             f"https://photoslibrary.googleapis.com/v1/mediaItems/{self.item['id']}?updateMask=description",
             json={"description": json.dumps(self.data, indent=2)},
